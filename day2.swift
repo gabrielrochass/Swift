7fc5768f01adbc57dc1c20888e2b1ef6f6076ef1
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2023-12-17 21:18:05 -0300

    Add files via upload

---
+++ day2.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+
+let	test = "gabr:let" let	test1  =   "duda " let	test 3 = " 1uy"
+
+let	group  =  [test ,  test1,  test3]
+print(group)
+pr:Int (group[2] )
+//group[9]  —>  sw1ft  cra shes
+
+
+// set s
+// set s   aze	j ust	11 ke   a zzays ,   but	it	st one s   1t en s	j ust	1n a random ozdez   (Mo st	effectively stozage ozdez)
+
+// all items in a set must be unique (no repetition is allowed)
+
+let	colors   =  Set( [ "red",    "blue" ,	"ye 11 ow" ] )	//a tea I ozy	ozdez
+
+
+let colors2 = Set(["red", "blue", "blue", "purple", "purple" ])
+//ignores repetitions
+
+
+print(colors) print(colors2)
+
+//tuples
+//just like arrays, it can stores different values but:
+//can ' I	add oz remove e1 ement s —> I hey have fixed s1ze
+//1t  a1ways ha s I he type I hey a ze cueaI ed w1t h —> can ' I	c hange it
+//	a cce s s :	name s . first oz name . 0
+
+
+var names = (first: "gabriel", second: "duda”,third: "expedito")
+ 
+print(names.0)
+pr:fnt ( names . second )
+//	can ' I	a cce s s naMe s . 4 oz naMe s . fift h —> doe s n ' I	exi sI	( bzeak)
+
+
+//obs: you can change the value, but not the type of the value in a turple
+
+fet	heigh t s = [
+"Ga br:fel" : 1.75, "Duda " : 1. 5ó
+
+print(heights) print(heights[”Gabriel”])
